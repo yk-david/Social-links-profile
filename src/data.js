3ae2921a616c77e023ce9c2c254ad25435c985d9
yk-david <81497136+yk-david@users.noreply.github.com>
2025-02-19 05:32:35 +0000

    minor url change in data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 const listArr = [
-  {name: 'GitHub', url: 'https://www.github.com'},
-  {name: 'Frontend Mentor', url: 'https://www.frontendmentor.io/profile/yk-david'},
-  {name: 'LinkedIn', url: 'https://www.linkedin.com'},
-  {name: 'Twitter', url: 'https://www.x.com'},
-  {name: 'Instagram', url: 'https:www.instagram.com'}
-]
+  { name: "GitHub", url: "https://www.github.com" },
+  {
+    name: "Frontend Mentor",
+    url: "https://www.frontendmentor.io/profile/yk-david",
+  },
+  { name: "LinkedIn", url: "https://www.linkedin.com" },
+  { name: "Twitter", url: "https://www.x.com" },
+  { name: "Instagram", url: "https://www.instagram.com" },
+];
 
 export default listArr;
